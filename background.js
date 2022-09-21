e526b7662852e8e9a9db1afa178ccceca72964ed
dirkwind <dirkwind707@gmail.com>
2022-09-21 08:32:57 -0500

    add questionable method for removing endscreens

--- background.js
+++ background.js
@@ -1,3 +1,18 @@
+function removeHomepage() {
+    for (const element of document.getElementsByTagName("ytd-two-column-browse-results-renderer")) {
+        element.remove()
+    }
+}
+
+function removeRecommended() {
+    document.getElementById("secondary").remove()
+
+    setTimeout(() => {
+        for (const element of document.getElementsByClassName("html5-endscreen")) {
+            element.remove()
+        }
+    }, 1000)
+}
 
 chrome.tabs.onUpdated.addListener(async (tabId, info) => {
     const tab = await chrome.tabs.get(tabId)
@@ -9,17 +24,12 @@ chrome.tabs.onUpdated.addListener(async (tabId, info) => {
     let func
 
     if (tab.url === "https://www.youtube.com/") {
-        func = () => {
-            for (const element of document.getElementsByTagName("ytd-two-column-browse-results-renderer")) {
-                element.remove()
-            }
-        }
+        func = removeHomepage
     } else if (tab.url.startsWith("https://www.youtube.com/watch")) {
-        func = () => {
-            document.getElementById("secondary").remove()
-        }
+        func = removeRecommended
     } else if (tab.url.startsWith("https://www.youtube.com/shorts/")) {
         chrome.tabs.update(tab.id, {url: `https://www.youtube.com/watch?v=${tab.url.split("/").pop()}`})
+        func = removeRecommended
     } else {
         return
     }
