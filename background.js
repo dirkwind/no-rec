b52725c467c4f9e10341ffc780f4893d41ad568b
dirkwind <dirkwind707@gmail.com>
2024-05-02 21:30:48 -0500

    format code, fix removeRecommended

--- background.js
+++ background.js
@@ -1,49 +1,54 @@
 const host = "https://www.youtube.com/";
 
 function makePath(path = "") {
-    return `${host}${path}`
+  return `${host}${path}`;
 }
 
 function removeHomepage() {
-    for (const element of document.getElementsByTagName("ytd-two-column-browse-results-renderer")) {
-        element.remove()
-    }
+  for (const element of document.getElementsByTagName(
+    "ytd-two-column-browse-results-renderer"
+  )) {
+    element.remove();
+  }
 }
 
 function removeRecommended() {
-    document.getElementById("secondary").remove()
+  document.getElementById("related").remove();
 
-    setTimeout(() => {
-        for (const element of document.getElementsByClassName("html5-endscreen")) {
-            element.remove()
-        }
-    }, 1000)
+  // timeout because apparently the endscreen isn't loaded immediately
+  setTimeout(() => {
+    document
+      .getElementsByClassName(
+        "html5-endscreen ytp-player-content videowall-endscreen"
+      )[0]
+      .remove();
+  }, 2000);
 }
 
 chrome.tabs.onUpdated.addListener(async (tabId, info) => {
-    const tab = await chrome.tabs.get(tabId)
-    
-    if (info.status !== "complete" || !tab.url.startsWith(host)) {
-        return
-    }
+  const tab = await chrome.tabs.get(tabId);
 
-    let func
+  if (info.status !== "complete" || !tab.url.startsWith(host)) {
+    return;
+  }
 
-    if (tab.url === host) {
-        func = removeHomepage
-    } else if (tab.url.startsWith(makePath("watch/"))) {
-        func = removeRecommended
-    } else if (tab.url.startsWith(makePath("shorts/"))) {
-        chrome.tabs.update(tab.id, {url: makePath(`watch?v=${tab.url.split("/").pop()}`)})
-        func = removeRecommended
-    } else {
-        return
-    }
-    
-    chrome.scripting.executeScript(
-        {
-            target: {tabId: tab.id},
-            func: func
-        }
-    )
-})
+  let func;
+
+  if (tab.url === host) {
+    func = removeHomepage;
+  } else if (tab.url.startsWith(makePath("watch"))) {
+    func = removeRecommended;
+  } else if (tab.url.startsWith(makePath("shorts"))) {
+    chrome.tabs.update(tab.id, {
+      url: makePath(`watch?v=${tab.url.split("/").pop()}`),
+    });
+    func = () => setTimeout(removeRecommended, 5000);
+  } else {
+    return;
+  }
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: func,
+  });
+});
